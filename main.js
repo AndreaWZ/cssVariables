394b65e05c35795597a2149836ed6eff543dc2e0
Andrea Wu <lixianhyun@gmail.com>
2020-10-19 00:18:50 -0700

    add EvenListener to buttons

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 window.onload = function(){
-
+    changeColor();
+    blurImage();
 };
+
+function changeColor(){
+    let image = document.getElementById('image');
+    let selectColor = document.getElementById('colors');
+    selectColor.addEventListener("click", function(){
+        console.log(selectColor.value);
+    });
+    // console.log(selectColor);
+};
+
+function blurImage(){
+    let image = document.getElementsByTagName("img");
+    let blurred = document.getElementById("blurred");
+    blurred.addEventListener("click", function(){
+        console.log(blurred.value);
+        // image.style.filter = "blur(" + blurred.value + "px)";
+    });
+};
+
