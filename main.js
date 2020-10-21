1424c6bf7d40fd3d00e47ad9b5098b5672bf94ea
Andrea Wu <lixianhyun@gmail.com>
2020-10-21 00:09:01 -0700

    remove background color for image and add border instead for spacing

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 window.onload = function(){
-    changeColor();
+    changeImageBorder();
     blurImage();
 };
 
-function changeColor(){
-    let image = document.getElementById('image');
+
+function changeImageBorder(){
+    let flowerImg = document.getElementById('flowerImg');
     let selectColor = document.getElementById('colors');
+    let borderSizes = document.getElementById("borderSizes");
     selectColor.addEventListener("change", function(){
-        image.style.backgroundColor = selectColor.value;
+        flowerImg.style.border = Number(borderSizes.value) + "px solid " + selectColor.value;
+    });
+    borderSizes.addEventListener("click", function(){
+        flowerImg.style.border = Number(borderSizes.value) + "px solid" + selectColor.value;
     });
 };
 
 function blurImage(){
-    let image = document.getElementById("flowerImage");
+    let image = document.getElementById('image');
     let blurred = document.getElementById("blurred");
     blurred.addEventListener("click", function(){
         image.style.filter = "blur(" + Number(blurred.value) + "px)";
